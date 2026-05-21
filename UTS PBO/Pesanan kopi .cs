00c3f04536f9fafb06f4734faddb84b520ceef64
ASUS <ASUS@LAPTOP-FEM4BSG6>
2026-05-22 00:27:48 +0700

    Add project files.

---
+++ UTS PBO/Pesanan kopi .cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.Marshalling;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace UTS_PBO
+{
+    private class Pesanan_kopi
+    {
+        private string namapelanggan;
+        private int nomorantrian;
+        private string menukopi;
+        private int jumlahcup;
+
+        // constructor 
+        public Pesanan_kopi(string namapelanggan, int nomorantrian, string menukopi, int jumlahcup)
+        {
+            this.namapelanggan = namapelanggan;
+            this.nomorantrian = nomorantrian;
+            this.menukopi = menukopi;
+            this.jumlahcup = jumlahcup;
+        }
+
+        // getter set dan setter 
+        public string getnamapelanggan() {
+            return namapelanggan;
+        }
+
+        public void setnamapelanggan()
+        {
+            this.namapelanggan = namapelanggan;
+        }
+
+        public int getnomorantrian() {
+            return nomorantrian;
+        }
+
+        public void setnomorantrian()
+        {
+            this.nomorantrian = nomorantrian;
+        }
+
+        public String getmenukopi() {
+            return menukopi;
+        }
+
+        public void setmenukopi()
+        {
+            this.menukopi = menukopi;
+        }
+
+        public int getjumlahcup() {
+            return jumlahcup;
+        }
+
+        public void setjumlahcup()
+        {
+            this.jumlahcup = jumlahcup;
+        }
+
+        public void tampilinfo()
+        {
+            Console.WriteLine("Nama Pelanggan : " + namapelanggan);
+            Console.WriteLine("Nomor Antrian : " + nomorantrian);
+            Console.WriteLine("Menu Kopi : " + menukopi);
+            Console.WriteLine("Jumlha cup : " + jumlahcup);
+        }
+
+        
+    }
+
+}
+
+
+    
+       
+
+
+
+
+    
---
+++ UTS PBO/PesananDineIn.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UTS_PBO
+{
+    internal class PesananDineIn
+    {
+    }
+}
